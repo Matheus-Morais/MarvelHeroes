690ecd23dc81321f9de2815b2cb82b0b901f9a41
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-11-12 12:09:34 -0300

    fix: adicionando os styles na pagina

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import {
+    Container,
+    Avatar,
+    Title,
+    Description
+} from './styles';
 
-export default function Hero() {
+export default function Hero({ item: hero }) {
+
     return (
-        <View>
-            <Text>Hello Hero</Text>
-        </View>
+        <Container>
+            <Avatar source={{ uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}` }} resizeMode="contain" />
+            <Title>{hero.name}</Title>
+            {hero.description != '' &&
+                <Description>{hero.description}</Description>
+            }
+        </Container>
     );
 }
