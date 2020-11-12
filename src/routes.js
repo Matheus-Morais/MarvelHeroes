2cfaa69289452bd16e7bdb5f4059c1a763cbe0b3
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-11-12 10:44:35 -0300

    feat: criação do arquivo de rotas

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import Heroes from './pages/Heroes';
+
+export default () => {
+    return (
+        <NavigationContainer>
+            <Navigator>
+                <Screen
+                    name="Heroes"
+                    component={Heroes}
+                />
+            </Navigator>
+        </NavigationContainer>
+    )
+}
