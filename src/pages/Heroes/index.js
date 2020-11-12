38c0d3b0c0a012d627bc300c31e3076c9e64478a
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-11-12 10:44:16 -0300

    feat: criação da pagina para herois

---
+++ src/pages/Heroes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Heroes() {
+    return (
+        <View>
+            <Text>Hello Heroes</Text>
+        </View>
+    );
+}
