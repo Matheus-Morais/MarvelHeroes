3b092f23d39eb1cec1d8d00b1872cc08ead48118
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-11-12 10:52:00 -0300

    feat: configurando reactotron

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 
+import './utils/Reactotron';
+
 import Routes from './routes';
 
 export default function Index() {
