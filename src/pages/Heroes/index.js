311607cbffb2b3543f3b2d2cab41ca6a4e9101ae
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-11-12 12:24:16 -0300

    fix: configurando o loading infinito

--- src/pages/Heroes/index.js
+++ src/pages/Heroes/index.js
@@ -10,13 +10,18 @@ import {
     Container,
     List,
     ContainerLoading,
-    Loading
+    Loading,
+    LoadingMoreDateItem
 } from './styles';
 
 export default function Heroes() {
 
     const [heroes, setHeroes] = useState([])
     const [loading, setLoading] = useState(true)
+
+    const [count, setCount] = useState(0);
+    const [totalResults, setTotalResults] = useState(0);
+    const [loadingMoreData, setLoadingMoreData] = useState(false);
 
     async function getHeroes() {
         setLoading(true)
@@ -27,16 +32,60 @@ export default function Heroes() {
 
         let response
         try {
-            response = await api.get(`/public/characters?nameStartsWith=a&ts=${ts}&apikey=e7501e8d4fcde60d7ff61baa795132fe&hash=${hash}`)
+            response = await api.get(`/public/characters?nameStartsWith=a&ts=${ts}&apikey=e7501e8d4fcde60d7ff61baa795132fe&hash=${hash}&limit=${20}&offset=${0}`)
         } catch (error) {
             Alert.alert('Oppss!', 'Não foi possível recuperar os dados do servidor da Marvel.')
             setLoading(false)
             return
         }
 
+        setCount(response.data.data.count)
+        setTotalResults(response.data.data.total)
         setHeroes(response.data.data.results)
         setLoading(false)
         return
+    }
+
+    async function loadingMoreHeroes() {
+        //verifica se ja existe ma solicitação
+        if (loadingMoreData) {
+            return;
+        }
+
+        //Verifica se ja foi atingido a méta
+        if (totalResults > 0 && heroes.length === totalResults) {
+            return
+        }
+
+        setLoadingMoreData(true)
+
+        //Ts e hash para enviar a solicitação
+        let ts = 1
+        let hash = md5(`${ts}${'29cd61407eb4fb9816a1f638275743698786dab3'}${'e7501e8d4fcde60d7ff61baa795132fe'}`)
+
+        let response
+        try {
+            response = await api.get(`/public/characters?nameStartsWith=a&ts=${ts}&apikey=e7501e8d4fcde60d7ff61baa795132fe&hash=${hash}&limit=${20}&offset=${count}`)
+        } catch (error) {
+            Alert.alert('Oppss!', 'Não foi possível recuperar mais dados do servidor da Marvel.')
+            setLoadingMoreData(false)
+            return
+        }
+
+        setCount(count + response.data.data.count)
+        // setTotalResults(response.data.data.total)
+        setHeroes([...heroes, ...response.data.data.results])
+        setLoadingMoreData(false)
+        return
+    }
+
+    function renderFooter() {
+        if (!loadingMoreData || heroes.length === totalResults) {
+            return null
+        } else {
+            return <LoadingMoreDateItem><Loading size="small" color="#000" /></LoadingMoreDateItem>
+        }
+
     }
 
     useEffect(() => {
@@ -52,6 +101,11 @@ export default function Heroes() {
                 <List
                     data={heroes}
                     keyExtractor={item => String(item.id)}
+                    refreshing={loading}
+                    onRefresh={getHeroes}
+                    onEndReached={loadingMoreHeroes}
+                    onEndReachedThreshold={0.2}
+                    ListFooterComponent={renderFooter}
                     renderItem={({ item }) => (
                         <Hero item={item} />
                     )}
