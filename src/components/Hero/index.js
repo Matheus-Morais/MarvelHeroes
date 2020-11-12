145b0f71f8ac6e117ed31f9518b21d2b05da5701
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-11-12 13:03:45 -0300

    fix: removendo descrição e melhorando os estilos

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 import {
     Container,
+    ContainerAvatar,
+    ContainerInfo,
     Avatar,
     Title,
-    Description
 } from './styles';
 
 export default function Hero({ item: hero }) {
 
     return (
         <Container>
-            <Avatar source={{ uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}` }} resizeMode="contain" />
-            <Title>{hero.name}</Title>
-            {hero.description != '' &&
-                <Description>{hero.description}</Description>
-            }
+            <ContainerAvatar>
+                <Avatar source={{ uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}` }} resizeMode="contain" />
+            </ContainerAvatar>
+
+            <ContainerInfo>
+                <Title>{hero.name}</Title>
+            </ContainerInfo>
         </Container>
     );
 }
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -5,29 +5,40 @@ const { height, width } = Dimensions.get("window");
 export const Container = styled.View`
     background: #f0f0f0;
 
-    justify-content: center;
+    flex-direction: row;
+
+    justify-content: space-evenly;
     align-items: center;
 
     padding: 5px;
+    margin-bottom: 7px;
+    margin-top: 7px;
 
-    margin-bottom: 15px;
+    border-width: 0.7px;
+`;
+
+export const ContainerAvatar = styled.View`
+    width: ${width * 0.35}px;
+    height: ${height * 0.2}px;
 `;
 
 export const Avatar = styled.Image`
-    width: ${height * 0.2}px;
+    width: ${width * 0.35}px;
     height: ${height * 0.2}px;
-    border-radius: ${height * 0.02}px;
+    border-radius: ${width * 0.02}px;
+`;
+
+export const ContainerInfo = styled.View`
+    width: ${width * 0.65}px;
+    height: ${height * 0.2}px;
+
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Title = styled.Text`
     font-size: ${width * 0.04}px;
     font-weight: bold;
-    color: #000;
-    text-align: center;
-`;
-
-export const Description = styled.Text`
-    font-size: ${width * 0.04}px;
     color: #000;
     text-align: center;
 `;
